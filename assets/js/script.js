48537627677d6127c806847d74fb0b4bd4bba4aa
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-11-05 00:58:39 -0400

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,11 @@
 var buttonEl = document.querySelector("#save-task");
-//console.log(buttonEl);
-var tasksToDoEL = document.querySelector("#tasks-to-do");
-
-var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", function() {
+var createTaskHandler = function() {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-});
+};
 
-
-var createTaskHandler = function() { 
-    var listItemEl = document.createElement("li"); 
-    listItemEl.className = "task-item"; 
-    listItemEl.textContent = "This is a new task."; 
-    tasksToDoEl.appendChild(listItemEl); 
-    };
-  
-  buttonEl.addEventListener("click", createTaskHandler);
-  //window.document.querySelector("button");
+buttonEl.addEventListener("click", createTaskHandler);
